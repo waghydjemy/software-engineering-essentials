e101a8fb5e83927d14e7a1219f8a79712bb50875
waghydjemy <wari.djemy@gmail.com>
2022-05-24 00:01:59 +0100

    add more data
    add two items
    add new colors

--- src/data.jsx
+++ src/data.jsx
@@ -8,12 +8,20 @@ const software_engineering_essentials = [
         items: ['Array', 'HashMap', 'Queue', 'Stack', 'Graphs', 'Tree', 'LinkedList', 'Heap', 'Tries', 'Recursion', 'Sorting', 'Searching', 'Dynamic Programming', 'Greedy Algorithms', 'Famous Algorithms', 'BackTracking', 'Sliding Window', 'Two pointers', 'Intervals', 'Math & Geometry', 'Bit Manipulation', 'Strings']
     },
     {
-        name: 'Distributed systems and databases',
-        items: ['Distributed storage ', 'Distributed computing', 'Sharding', 'Partitioning', 'Load balancing', 'SQL', 'NoSQL', 'Data Modelling', 'Index', 'ACID']
+        name: 'Programing Paradigms',
+        items: ['Imperative', 'Declarative', 'Object oriented', 'Functional', 'Logical']
     },
     {
-        name: 'DSA ( Data Structures & Algorithms )',
-        items: ['Array', 'HashMap', 'Queue', 'Stack', 'Graphs', 'Tree', 'LinkedList', 'Heap', 'Tries', 'Recursion', 'Sorting', 'Searching', 'Dynamic Programming', 'Greedy Algorithms', 'Famous Algorithms', 'BackTracking', 'Sliding Window', 'Two pointers', 'Intervals', 'Math & Geometry', 'Bit Manipulation', 'Strings']
+        name: 'Security',
+        items: ['OWASP top 10', 'JWT', 'OAth', 'SSO', 'SAML', 'OpenID', 'CORS', 'HTTPS', 'SSL', 'TLS', 'SSH']
+    },
+    {
+        name: 'Documentation',
+        items: ['Markup language', 'OpenAPI', 'Swagger', 'Wireframing']
+    },
+    {
+        name: 'Testing',
+        items: ['Unit Testing','Integration testing','Load Testing','Stress Testing','Test Automation','Mutation Testing']
     }
 ]
 
